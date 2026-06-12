f024bb9847615f718d6317a405572dd7d66ec593
Arman Abasian <abasian.arman@gmail.com>
2026-06-12 19:54:18 +0330

    some changes

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -3,12 +3,12 @@
   <div class="flex w-screen h-screen">
     <div class="w-64 bg-primary overflow-auto pt-4">
       <ul class="flex flex-col gap-4 px-4">
-        <li>آمارها</li>
+        <li>خانه</li>
         <li>پزشکان</li>
         <li>کاربران</li>
         <li>نظرات</li>
       </ul>
     </div>
-    <div class="flex-1 overflow-auto pt-4">کاربران</div>
+    <div class="flex-1 overflow-auto pt-4"><slot /></div>
   </div>
 </template>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
 definePageMeta({ layout: 'admin' })
 </script>
-<template></template>
+<template>
+  <div class="flex items-start gap-4 justify-center">
+    <div
+      class="w-64 h-28 rounded-md flex justify-center items-center bg-primary"
+    >
+      تعداد کاربران :250
+    </div>
+    <div
+      class="w-64 h-28 rounded-md flex justify-center items-center bg-primary"
+    >
+      تعداد پزشکان :250
+    </div>
+    <div
+      class="w-64 h-28 rounded-md flex justify-center items-center bg-primary"
+    >
+      تعداد رزروها :250
+    </div>
+  </div>
+</template>
